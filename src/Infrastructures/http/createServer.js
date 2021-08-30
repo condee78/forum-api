bafcf3541dcf1ccc8c8e63285e14cdc33b526ccb
condee78 <condee78@gmail.com>
2021-08-30 16:14:53 +0700

    Create Threads, criteria 1

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,13 +1,19 @@
-const Hapi = require('@hapi/hapi');
-const ClientError = require('../../Commons/exceptions/ClientError');
-const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
-const users = require('../../Interfaces/http/api/users');
-const authentications = require('../../Interfaces/http/api/authentications');
+const Hapi = require("@hapi/hapi");
+const ClientError = require("../../Commons/exceptions/ClientError");
+const DomainErrorTranslator = require("../../Commons/exceptions/DomainErrorTranslator");
+const users = require("../../Interfaces/http/api/users");
+const authentications = require("../../Interfaces/http/api/authentications");
+const threads = require("../../Interfaces/http/api/threads");
 
 const createServer = async (injections) => {
   const server = Hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
   });
 
   await server.register([
@@ -19,11 +25,16 @@ const createServer = async (injections) => {
       plugin: authentications,
       options: { injections },
     },
+    {
+      plugin: threads,
+      options: { injections },
+    },
   ]);
 
-  server.ext('onPreResponse', (request, h) => {
+  server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
+    // console.log(request);
 
     if (response instanceof Error) {
       // bila response tersebut error, tangani sesuai kebutuhan
@@ -32,7 +43,7 @@ const createServer = async (injections) => {
       // penanganan client error secara internal.
       if (translatedError instanceof ClientError) {
         const newResponse = h.response({
-          status: 'fail',
+          status: "fail",
           message: translatedError.message,
         });
         newResponse.code(translatedError.statusCode);
@@ -46,8 +57,8 @@ const createServer = async (injections) => {
 
       // penanganan server error sesuai kebutuhan
       const newResponse = h.response({
-        status: 'error',
-        message: 'terjadi kegagalan pada server kami',
+        status: "error",
+        message: "terjadi kegagalan pada server kami",
       });
       newResponse.code(500);
       return newResponse;
--- src/Infrastructures/injections.js
+++ src/Infrastructures/injections.js
@@ -1,25 +1,30 @@
 /* istanbul ignore file */
 
 // external agency
-const { nanoid } = require('nanoid');
-const bcrypt = require('bcrypt');
-const Jwt = require('@hapi/jwt');
-const pool = require('./database/postgres/pool');
+const { nanoid } = require("nanoid");
+const bcrypt = require("bcrypt");
+const Jwt = require("@hapi/jwt");
+const pool = require("./database/postgres/pool");
 
 // service (repository, helper, manager, etc)
-const UserRepositoryPostgres = require('./repository/UserRepositoryPostgres');
-const AuthenticationRepositoryPostgres = require('./repository/AuthenticationRepositoryPostgres');
-const BcryptEncryptionHelper = require('./security/BcryptEncryptionHelper');
-const JwtTokenManager = require('./security/JwtTokenManager');
+const UserRepositoryPostgres = require("./repository/UserRepositoryPostgres");
+const AuthenticationRepositoryPostgres = require("./repository/AuthenticationRepositoryPostgres");
+const BcryptEncryptionHelper = require("./security/BcryptEncryptionHelper");
+const JwtTokenManager = require("./security/JwtTokenManager");
+
+const ThreadRepositoryPostgres = require("./repository/ThreadRepositoryPostgres");
 
 // use case
-const AddUserUseCase = require('../Applications/use_case/AddUserUseCase');
-const LoginUserUseCase = require('../Applications/use_case/LoginUserUseCase');
-const RefreshAuthenticationUseCase = require('../Applications/use_case/RefreshAuthenticationUseCase');
-const LogoutUserUseCase = require('../Applications/use_case/LogoutUserUseCase');
+const AddUserUseCase = require("../Applications/use_case/AddUserUseCase");
+const LoginUserUseCase = require("../Applications/use_case/LoginUserUseCase");
+const RefreshAuthenticationUseCase = require("../Applications/use_case/RefreshAuthenticationUseCase");
+const LogoutUserUseCase = require("../Applications/use_case/LogoutUserUseCase");
+
+const AddThreadUseCase = require("../Applications/use_case/AddThreadUseCase");
 
 const serviceInstanceContainer = {
   userRepository: new UserRepositoryPostgres(pool, nanoid),
+  threadRepository: new ThreadRepositoryPostgres(pool, nanoid),
   authenticationRepository: new AuthenticationRepositoryPostgres(pool),
   encryptionHelper: new BcryptEncryptionHelper(bcrypt),
   authenticationTokenManager: new JwtTokenManager(Jwt.token),
@@ -30,15 +35,23 @@ const useCaseInstanceContainer = {
     userRepository: serviceInstanceContainer.userRepository,
     encryptionHelper: serviceInstanceContainer.encryptionHelper,
   }),
+  addThreadUseCase: new AddThreadUseCase({
+    threadRepository: serviceInstanceContainer.threadRepository,
+    authenticationTokenManager:
+      serviceInstanceContainer.authenticationTokenManager,
+  }),
+
   loginUserUseCase: new LoginUserUseCase({
     authenticationRepository: serviceInstanceContainer.authenticationRepository,
-    authenticationTokenManager: serviceInstanceContainer.authenticationTokenManager,
+    authenticationTokenManager:
+      serviceInstanceContainer.authenticationTokenManager,
     userRepository: serviceInstanceContainer.userRepository,
     encryptionHelper: serviceInstanceContainer.encryptionHelper,
   }),
   refreshAuthenticationUseCase: new RefreshAuthenticationUseCase({
     authenticationRepository: serviceInstanceContainer.authenticationRepository,
-    authenticationTokenManager: serviceInstanceContainer.authenticationTokenManager,
+    authenticationTokenManager:
+      serviceInstanceContainer.authenticationTokenManager,
   }),
   logoutUserUseCase: new LogoutUserUseCase({
     authenticationRepository: serviceInstanceContainer.authenticationRepository,
